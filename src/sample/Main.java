43d0f122e18385f6c988d207e26d03e80e66155c
Guillaume Robert <Guillaume.Robert@etu.udamail.fr>
2015-11-13 22:09:56 +0100

    Ajout d'une constante pour zoomer la carte

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,26 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import org.nocrala.tools.gis.data.esri.shapefile.ShapeFileReader;
-import org.nocrala.tools.gis.data.esri.shapefile.ValidationPreferences;
 import org.nocrala.tools.gis.data.esri.shapefile.exception.InvalidShapeFileException;
-import org.nocrala.tools.gis.data.esri.shapefile.header.ShapeFileHeader;
 import org.nocrala.tools.gis.data.esri.shapefile.shape.AbstractShape;
 import org.nocrala.tools.gis.data.esri.shapefile.shape.PointData;
-import org.nocrala.tools.gis.data.esri.shapefile.shape.ShapeType;
 import org.nocrala.tools.gis.data.esri.shapefile.shape.shapes.PolygonShape;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main extends Application {
+
+    private static final double ZOOM = 5.4;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -54,17 +53,17 @@ public class Main extends Application {
             e.printStackTrace();
             System.exit(3);
         }
-        double x = Math.abs(reader.getHeader().getBoxMaxX()) + Math.abs(reader.getHeader().getBoxMinX());
-        double y = Math.abs(reader.getHeader().getBoxMaxY()) + Math.abs(reader.getHeader().getBoxMinY());
-        double minX = reader.getHeader().getBoxMinX();
-        double minY = reader.getHeader().getBoxMinY();
+        double x = (Math.abs(reader.getHeader().getBoxMaxX()) - reader.getHeader().getBoxMinX()) * ZOOM;
+        double y = (Math.abs(reader.getHeader().getBoxMaxY()) - reader.getHeader().getBoxMinY()) * ZOOM;
+        double minX = reader.getHeader().getBoxMinX() * ZOOM;
+        double minY = reader.getHeader().getBoxMinY() * ZOOM;
         System.out.println("min (X) : " + minX);
-        System.out.println("max (X) : " + reader.getHeader().getBoxMaxX());
+        System.out.println("max (X) : " + reader.getHeader().getBoxMaxX() * ZOOM);
         System.out.println("size (X) : " + x);
         System.out.println("min (Y) : " + minY);
-        System.out.println("max (Y) : " + reader.getHeader().getBoxMaxY());
+        System.out.println("max (Y) : " + reader.getHeader().getBoxMaxY() * ZOOM);
         System.out.println("size (Y) : " + y);
-        if((Math.abs(reader.getHeader().getBoxMaxZ()) + Math.abs(reader.getHeader().getBoxMinZ())) != 0 || (Math.abs(reader.getHeader().getBoxMaxM()) + Math.abs(reader.getHeader().getBoxMinM())) != 0){
+        if(reader.getHeader().getBoxMaxZ() != 0 || reader.getHeader().getBoxMinZ() != 0 || reader.getHeader().getBoxMaxM() != 0 || reader.getHeader().getBoxMinM() != 0){
             System.out.println("Format de carte invalide");
             System.exit(4);
         }
@@ -75,37 +74,28 @@ public class Main extends Application {
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         gc.setStroke(Color.BLACK);
-        gc.setLineWidth(2);
-        gc.setFill(Color.BLUE);
+        gc.setLineWidth(1);
         System.out.println("Shape File : " + reader.getHeader().getShapeType().name());
 
-        AbstractShape shape = null;
+        AbstractShape shape;
         int nb = 0;
         PointData previous = null;
         while ((shape = reader.next()) != null)
         {
             PolygonShape polygon = (PolygonShape) shape;
-            /*System.out.println("I read a Polygon with "
-                    + polygon.getNumberOfParts() + " parts and "
-                    + polygon.getNumberOfPoints() + " points");*/
             for (int i = 0; i < polygon.getNumberOfParts(); i++) {
                 PointData[] points = polygon.getPointsOfPart(i);
-                /*System.out.println("- part " + i + " has " + points.length
-                        + " points.");*/
                 for(PointData point : points)
                 {
                     if(previous != null)
                     {
-                        gc.strokeLine(previous.getX()+Math.abs(minX),y-(previous.getY()+Math.abs(minY)),point.getX()+Math.abs(minX),y-(point.getY()+Math.abs(minY)));
+                        gc.strokeLine(previous.getX() * ZOOM - minX, y - (previous.getY() * ZOOM - minY), point.getX() * ZOOM - minX,y - (point.getY() * ZOOM - minY));
                     }
                     previous = point;
                 }
                 previous = null;
             }
             nb++;
-            //System.out.println(shape.getHeader().getContentLength());
-            //System.out.println(shape.getHeader().getRecordNumber());
-            //System.out.println(shape.getHeader().getContentLength());
 
         }
         System.out.println("nb :" + nb);
