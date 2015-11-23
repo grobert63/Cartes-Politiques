7d6d03118a096fbaab14b34509bb1bbb7d24af6c
Guillaume Robert <Guillaume.Robert@etu.udamail.fr>
2015-11-23 20:24:47 +0100

    Revert "déplacement de la fenêtre dans un nouveau package Window"
    This reverts commit d898f5d9f9c1ea9634b8e16a300b763d5eda5b72.

--- src/Window/Controller.java
+++ src/Window/Controller.java
@@ -1,7 +1,25 @@
 package Window;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    Label label;
+
+    @FXML
+    Button button;
+
     void initialize()
     {
+    }
+
+    public void buttonClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
     }
 }
