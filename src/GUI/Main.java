4ef22e98311fb88bcb648d9ffc103b0c8533ca36
Julien <unuldur.seladon@laposte.net>
2016-03-13 10:13:06 +0100

    argument ajouter  +algo meilleur ??

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -46,7 +46,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         LoggerManager.getInstance().getLogger().log(Level.INFO, "Starting...");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hexagomap");
         primaryStage.setScene(new Scene(root, 650, 400));
         primaryStage.show();
         LoggerManager.getInstance().getLogger().log(Level.INFO, "Application started");
